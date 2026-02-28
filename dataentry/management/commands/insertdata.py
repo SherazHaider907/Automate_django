a31bdf67ecb3cf8684832156cb75475b65a38ca2
SherazHaider907 <Sherazhaider665@gmail.com>
2026-02-28 13:50:59 +0500

    add simple validations

--- dataentry/management/commands/insertdata.py
+++ dataentry/management/commands/insertdata.py
@@ -15,7 +15,16 @@ class Command(BaseCommand):
             {"name": "Smith", "age": 23, "roll_number": "4"},
             {"name": "Emily", "age": 19, "roll_number": "5"},
             {"name": "Michael", "age": 24, "roll_number": "6"},
+            {"name": "Sarah", "age": 20, "roll_number": "7"},
+            {"name": "David", "age": 22, "roll_number": "8"},
+            {"name": "Jessica", "age": 21, "roll_number": "9"},
+            {"name": "Daniel", "age": 23, "roll_number": "10"},
         ]
         for data in dataset:
-            Student.objects.create(name=data["name"], age=data["age"], roll_number=data["roll_number"])
+            roll_no=data["roll_number"]
+            existing_record = Student.objects.filter(roll_number=roll_no).exists()
+            if not existing_record:
+                Student.objects.create(name=data["name"], age=data["age"], roll_number=data["roll_number"])
+            else:
+                self.stdout.write(self.style.WARNING(f"Record with roll number {roll_no} already exists."))
         self.stdout.write(self.style.SUCCESS("Data inserted successfully"))
