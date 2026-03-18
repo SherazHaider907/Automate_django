4ce2d70401946a0d51981bb913cc6998c22b1cb2
SherazHaider907 <Sherazhaider665@gmail.com>
2026-03-18 13:39:10 +0500

    add celery and redis

--- awd_main/views.py
+++ awd_main/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def home(request):
     # The template is stored under templates/dataentry/home.html
     return render(request, "dataentry/home.html")
+
+def celery_test(request):
+    return HttpResponse('hi this is celery test')
