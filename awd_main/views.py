555a72c125b225637eef46009c3f77fd537dc376
SherazHaider907 <Sherazhaider665@gmail.com>
2026-03-23 19:12:59 +0500

    frontend layout

--- awd_main/views.py
+++ awd_main/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from dataentry.tasks import celery_test_task
 def home(request):
     # The template is stored under templates/dataentry/home.html
-    return render(request, "dataentry/home.html")
+    return render(request, "home.html")
 
 def celery_test(request):
     celery_test_task.delay()
