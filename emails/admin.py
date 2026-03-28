534b727e0f2e52a437ba06aa34c778a20ff1dea1
SherazHaider907 <Sherazhaider665@gmail.com>
2026-03-28 22:51:29 +0500

    list subscriber email models

--- emails/admin.py
+++ emails/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import List,Subcriber,Email
 # Register your models here.
+
+admin.site.register(List)
+admin.site.register(Subcriber)
+admin.site.register(Email)
