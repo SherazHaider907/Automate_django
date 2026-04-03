260f198f72b589da186d93973bf0b3612e7d1343
SherazHaider907 <Sherazhaider665@gmail.com>
2026-04-03 19:53:46 +0500

    implementing autocomplete form

--- stockanalysis/views.py
+++ stockanalysis/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
-
+from dal import autocomplete
+from .models import Stock
+from .forms import StockForm
 # Create your views here.
 def stocks(request):
-    return render(request, "stockanalysis/stocks.html")
+    form = StockForm()
+    context = {
+        'form': form
+    }
+    return render(request, "stockanalysis/stocks.html",context)
+
+
+class StockAutocomplete(autocomplete.Select2QuerySetView):
+    def get_queryset(self):
+        qs = Stock.objects.all()
+
+        if self.q:
+            qs = qs.filter(name__icontains=self.q)
+
+        return qs
